25837c0771d03f6463b263908134c0cb7cc7538f
Iaroslava L <66276044+Iara-L@users.noreply.github.com>
2022-06-15 09:56:24 +0300

    task 3

---
+++ 3.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void codedstr(char* str){
+    int i = 0;
+
+    while(str[i] != '\0'){
+        if(i % 2 == 0){
+            str[i] = 'A' + (str[i] - '0');
+        }
+        else if(i % 2 != 0){
+            switch (str[i])
+            {
+            case '0':
+                str[i] = '!';
+                break;
+            case '1':
+                str[i] = '#';
+                break;
+            case '2':
+                str[i] = '/';
+                break;
+            case '3':
+                str[i] = '~';
+                break;
+            case '4':
+                str[i] = '=';
+                break;
+            case '5':
+                str[i] = '`';
+                break;
+            case '6':
+                str[i] = '\\';
+                break;
+            case '7':
+                str[i] = '>';
+                break;
+            case '8':
+                str[i] = '.';
+                break;
+            case '9':
+                str[i] = ',';
+                break;
+            default:
+                printf("Error\n");
+            }
+        }
+        i++;
+    }
+    printf("%s\n", str);
+}
+
+int main(){
+    char str[501];
+
+    scanf("%s", str);
+
+    codedstr(str);
+
+    printf("\n");
+
+    return 0;
+}
